4b37e894a31c101b7d6d7446b2b8e43ac5786b9f
Adam Bedient <adambedient@gmail.com>
2023-11-10 09:22:53 -0500

    Completes challenge 1

--- ifStatements.js
+++ ifStatements.js
@@ -8,10 +8,12 @@
 // Write a conditional statement that prints only one or the other.
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
-var numQuarters = 0;
-
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
+var numQuarters = 2;
+if (numQuarters >= 2){
+    console.log("I have enough money for a gumball");
+} else {
+    console.log("I don't have enough money for a gumball");
+}
 
 
 // CHALLENGE 2
